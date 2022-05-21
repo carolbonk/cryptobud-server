357f6928661c02d6227bf0fc4d7c050c6bdd7c82
Carolaine Bonk Sarmento <carolbonk@Carolaines-MacBook-Pro.local>
2022-05-20 23:05:26 -0400

    Improves post filters

--- routes/posts.js
+++ routes/posts.js
@@ -1,121 +1,149 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const fs = require('fs');
-const crypto = require('crypto');
-const { response } = require('express');
-const jwt = require('jsonwebtoken');
-const knex = require('knex')(require('../knexfile'));
+const fs = require("fs");
+const crypto = require("crypto");
+const { response } = require("express");
+const jwt = require("jsonwebtoken");
+const knex = require("knex")(require("../knexfile"));
 
+router.get("/", (req, res) => {
+  const authToken = req.headers.authorization.split(" ")[1];
+  jwt.verify(authToken, process.env.JWT_KEY, (err, decoded) => {
+    if (err) {
+      console.log(err);
+      return res.status(401).send("Invalid auth token");
+    }
 
-router.get ('/', (req, res) => {
-    const authToken = req.headers.authorization.split(" ")[1];
-    jwt.verify(authToken, process.env.JWT_KEY, (err, decoded) => {
+    console.log("to " + req.query.to);
+    let where = null;
+    let orWhere = null;
+    let orWhereTwo = null;
+    if (!req.query.user_id) {
+      where = { global: true };
+      orWhere = { global: false, primary_user_id: decoded.id };
+      orWhereTwo = { global: false, user_id: decoded.id };
 
-        if (err) {
-            console.log(err);
-            return res.status(401).send("Invalid auth token");
-        }
-       
-        console.log("to " + req.query.to);
-      let where = null;
-
-      if (!req.query.user_id)
-      {
-        where = { global: true };
-      }
-      else
-      {
-          where = {user_id: req.query.user_id, global:true};
-      }
-
-      knex('post').join('user', 'post.user_id', 'user.id') 
-       .where(where)
-       .select('post.message', 'post.user_id', 'post.id', 'post.date', 'post.global', 
-        'post.image_url', 'user.first_name', 'user.last_name', 'user.avatar_url')
-       .orderBy('date', 'desc')
-       .limit(req.query.to)
-       .offset(req.query.from)
-       .then((posts) => {
-
+      knex("post")
+        .join("user", "post.user_id", "user.id")
+        .join("following", "post.user_id", "following.secondary_user_id")
+        .where(where)
+        .orWhere(orWhere)
+        .orWhere(orWhereTwo)
+        .select(
+          "post.message",
+          "post.user_id",
+          "post.id",
+          "post.date",
+          "post.global",
+          "post.image_url",
+          "user.first_name",
+          "user.last_name",
+          "user.avatar_url"
+        )
+        .orderBy("date", "desc")
+        .distinct("post.id")
+        .limit(req.query.to)
+        .offset(req.query.from)
+        .then((posts) => {
           let data = {
-               posts: posts
-              } ;
+            posts: posts,
+          };
+          res.status(201).send(data);
+        });
+    } else {
+      where = { user_id: req.query.user_id, global: true };
+      orWhere = {
+        user_id: req.query.user_id,
+        global: false,
+        primary_user_id: decoded.id,
+      };
+
+      knex("post")
+      .join("user", "post.user_id", "user.id")
+      .join("following", "post.user_id", "following.secondary_user_id")
+      .where(where)
+      .orWhere(orWhere)
+      .select(
+        "post.message",
+        "post.user_id",
+        "post.id",
+        "post.date",
+        "post.global",
+        "post.image_url",
+        "user.first_name",
+        "user.last_name",
+        "user.avatar_url"
+      )
+      .orderBy("date", "desc")
+      .distinct("post.id")
+      .limit(req.query.to)
+      .offset(req.query.from)
+      .then((posts) => {
+        let data = {
+          posts: posts,
+        };
         res.status(201).send(data);
+      });
     }
-    );
-});
+  });
 });
 
-router.post('/', (req, res) => {
-    const authToken = req.headers.authorization.split(" ")[1];
-    jwt.verify(authToken, process.env.JWT_KEY, (err, decoded) => {
-        if (err) {
-            console.log(err);
-            return res.status(401).send("Invalid auth token");
-        }
-    const {message, global, image, image_type} = req.body;
+router.post("/", (req, res) => {
+  const authToken = req.headers.authorization.split(" ")[1];
+  jwt.verify(authToken, process.env.JWT_KEY, (err, decoded) => {
+    if (err) {
+      console.log(err);
+      return res.status(401).send("Invalid auth token");
+    }
+    const { message, global, image, image_type } = req.body;
     const user_id = decoded.id;
-   
-    if (!global) {
 
-       
-        return res.status(400).send("Please enter the required fields.");
+    if (global == null) {
+      return res.status(400).send("Please enter the required fields.");
     }
-
 
     //at least one of these is required
     if (!message && (!image || !image_type)) {
-
-       
-        return res.status(400).send("Please enter the required fields.");
+      return res.status(400).send("Please enter the required fields.");
     }
 
-            let newPost = null;
-            if (!!image && !!image_type)
-            {
-            
-            let binaryData = Buffer.from(image, 'base64').toString('binary');
-        
-            let fileName= crypto.randomUUID() + "." + image_type;
-        
-            fs.writeFileSync("public/images/" + fileName, binaryData, "binary");
-            
-            
-            let urlPrefix = process.env.BACKEND_URL + ":" + process.env.PORT + "/images/";
-        
-            newPost = {
-               message: message,
-               user_id: user_id,
-               image_url: urlPrefix + fileName,
-               global: global
-            }
-          }
-          else
-          {
-                 
-            newPost = {
-                message: message,
-                user_id: user_id,
-                global: global
-          }
-        
-            // Create the new post
-  
-            knex('post')
-                .insert(newPost)
-                .then(() => {
-                    res.status(201).send("Posted successfully");
-                })
-                .catch((error) => {
-                    res.status(400).send("Failed posting");
-                    console.log(error);
-                });
-        
-    }});
-   
-}
-);
+    let newPost = null;
+    if (!!image && !!image_type) {
+      let binaryData = Buffer.from(image, "base64").toString("binary");
 
+      let fileName = crypto.randomUUID() + "." + image_type;
 
+      fs.writeFileSync("public/images/" + fileName, binaryData, "binary");
+
+      let urlPrefix =
+        process.env.BACKEND_URL + ":" + process.env.PORT + "/images/";
+
+      newPost = {
+        message: message,
+        user_id: user_id,
+        image_url: urlPrefix + fileName,
+        global: global,
+      };
+    } else {
+      newPost = {
+        message: message,
+        user_id: user_id,
+        global: global,
+      };
+
+      // Create the new post
+
+      knex("post")
+        .insert(newPost)
+        .then(() => {
+          res.status(201).send("Posted successfully");
+        })
+        .catch((error) => {
+          res.status(400).send("Failed posting");
+          console.log(error);
+        });
+    }
+  });
+});
 
 module.exports = router;
