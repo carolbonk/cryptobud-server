7333e6d346992c229f3b69b44482252a67007b91
Carolaine Bonk Sarmento <carolbonk@Carolaines-MacBook-Pro.local>
2022-05-26 23:08:55 -0400

    Fixes deployment issue with knexfile

--- knexfile.js
+++ knexfile.js
@@ -9,8 +9,9 @@ const connections = {development: {
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     user:     process.env.DB_USER,
-    password: process.env.DB_PASS
-  }},
+    password: process.env.DB_PASS,
+  },
+},
   production: {
     client: 'mysql',
     connection: process.env.JAWSDB_URL,
